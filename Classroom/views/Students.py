e984f9c127b441dd3d35b1ecf4fbef1e4a6f2d12
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2025-08-03 20:53:38 +0530

    change the static and templates

---
+++ Classroom/views/Students.py
@@ -0,0 +1,246 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from ..decorators import student_required
+from ..models import Classroom, Enrollment, Test, Question, Answer, testTaken
+import datetime
+from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
+from sklearn.feature_extraction.text import TfidfTransformer
+import nltk, string, numpy, math
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+@login_required(login_url='login')
+@student_required
+def join_class(request):
+	if request.method == "POST":
+		code = request.POST['code']
+		user = request.user 
+
+		try:
+			room = Classroom.objects.get(code=code)
+		except Classroom.DoesNotExist:
+			messages.warning(request, "There's no such Classroom")
+			return redirect('join_class')
+
+		if Enrollment.objects.filter(room=room, student=user).exists():
+			messages.info(request, 'You Already Enrolled {}'.format(room))
+		else: 
+			Enrollment(room=room, student=user).save() 
+			messages.success(request, '{} Class Enrolled'.format(room))
+
+		return redirect('dashboard')
+
+	return render(request, 'students/join_class.html')
+
+
+@login_required(login_url='login')
+@student_required
+def attend_test(request, test_id):
+	test = get_object_or_404(Test, id=test_id)
+
+	if testTaken.objects.filter(test=test, student=request.user).exists():
+		return redirect('review_test', test_id)
+
+	qns = Question.objects.filter(test=test_id)
+	return render(request, 'students/attend_test.html', { 'qns' : qns, 'test' : test } )
+
+
+
+
+@login_required(login_url='login')
+@student_required
+def submit_test(request, test_id):
+	nltk.data.path.append('../../nltk_data/') # For Lookup 
+	
+	qns = Question.objects.filter(test=test_id)
+	test = get_object_or_404(Test, id=test_id)
+	student = request.user 
+
+	if testTaken.objects.filter(test=test, student=request.user).exists():
+		return redirect('review_test', test_id)
+
+	tt = testTaken(test=test, student=student, actual_score=0, ml_score=0)
+	tt.save()
+
+	for q in qns:
+		id = str(q.id)
+		ans = request.POST[id] 
+		ans = Answer(student=student, question=q, answer_text=ans)
+
+		def LemTokens(tokens):
+			return [lemmer.lemmatize(token) for token in tokens]
+
+		def LemNormalize(text):
+			tokens = nltk.word_tokenize(text)
+			words = [w.lower() for w in tokens if w.isalnum()]
+			return LemTokens(words)
+
+		def cos_similarity(textlist):
+			tfidf = TfidfVec.fit_transform(textlist)
+			return (tfidf * tfidf.T).toarray()
+
+		
+		answer_key = q.key 		
+		answer = ans.answer_text
+
+		documents = [answer_key,answer]
+		documents = list(map(str, documents))
+		lemmer = nltk.stem.WordNetLemmatizer()
+
+		TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+
+		tf_matrix = cos_similarity(documents)
+		tfidfTran = TfidfTransformer(norm="l2")
+		tfidfTran.fit(tf_matrix)
+		tfidf_matrix = tfidfTran.transform(tf_matrix)
+		cos_similarity_matrix = (tfidf_matrix * tfidf_matrix.T).toarray()
+		score = cos_similarity_matrix[0][1]*q.max_score
+		mark = int(round(score))
+		
+		ans.actual_score = mark 
+		ans.ml_score = mark 
+		ans.save()
+		tt.actual_score += ans.actual_score
+		tt.ml_score += ans.ml_score
+
+	tt.save()
+
+	return redirect('view_class', test.belongs.id)
+
+@login_required(login_url='login')
+@student_required
+def review_test(request, test_id):
+	test = get_object_or_404(Test, id=test_id)
+	qns = Question.objects.filter(test=test_id)
+	student = request.user 
+	ans = []
+	tot = 0
+	act = 0  
+	for i in range(len(qns)) :
+		d = {} 
+		d['qns'] = qns[i] 
+		d['ans'] = get_object_or_404(Answer, student=student, question=qns[i]) # Answer.objects.filter(student=student, question=qns[i]) 
+		ans.append(d)
+		act += d['ans'].actual_score  
+		tot += qns[i].max_score 
+
+	mark = "{} / {}".format(act, tot)
+	return render(request, 'students/review_test.html', { 'test' : test, 'ans' : ans, 'mark' : mark })
+
+
+@login_required(login_url='login')
+@student_required
+def assigned_test(request, class_id):
+	test = Test.objects.filter(belongs=class_id)
+
+	tests = []
+	for t in test:
+		if(testTaken.objects.filter(test=t, student=request.user).exists()):
+			continue 
+		elif ( t.start_time == None or t.start_time < timezone.now()) and ( t.end_time == None or t.end_time > timezone.now()):
+			t.status = "Assigned"
+			tests.append(t)
+
+	# Search
+	search = request.GET.get('search')
+
+	if search != "" and search is not None:
+		tests = Test.objects.filter(belongs=class_id, name__icontains=search).order_by('-create_time')
+
+
+	# paginator 
+	paginator = Paginator(tests, 5)
+	page = request.GET.get('page', 1)
+
+	try:
+		tests = paginator.page(page)
+	except PageNotAnInteger:
+		tests = paginator.page(1)
+	except EmptyPage:
+		tests = paginator.page(paginator.num_pages)
+
+		
+	room = get_object_or_404(Classroom, id=class_id)
+	return render(request, 'classroom/view_class.html', {'tests' : tests, 'room' : room } )
+
+
+@login_required(login_url='login')
+@student_required
+def missing_test(request, class_id):
+	test = Test.objects.filter(belongs=class_id)
+
+	tests = []
+	for t in test:
+		if(testTaken.objects.filter(test=t, student=request.user).exists()):
+			continue 
+		elif ( t.start_time == None or t.start_time < timezone.now()) and ( t.end_time == None or t.end_time > timezone.now()):
+			t.status = "Assigned"
+			tests.append(t)
+		elif(t.start_time and t.start_time > timezone.now()): # test is not yet started
+			t.status = "not"
+		else:
+			t.status = "late"
+			tests.append(t)
+
+	# Search
+	search = request.GET.get('search')
+
+	if search != "" and search is not None:
+		tests = Test.objects.filter(belongs=class_id, name__icontains=search).order_by('-create_time')
+
+
+	# paginator 
+	paginator = Paginator(tests, 5)
+	page = request.GET.get('page', 1)
+
+	try:
+		tests = paginator.page(page)
+	except PageNotAnInteger:
+		tests = paginator.page(1)
+	except EmptyPage:
+		tests = paginator.page(paginator.num_pages)
+
+	room = get_object_or_404(Classroom, id=class_id)
+	return render(request, 'classroom/view_class.html', {'tests' : tests, 'room' : room } )
+
+
+@login_required(login_url='login')
+@student_required
+def done_test(request, class_id):
+	taken = list(testTaken.objects.filter(student=request.user).values("test"))
+
+	d = []
+	for t in taken: 
+		d.append( t['test'] )
+	tests = Test.objects.filter(pk__in=d, belongs=class_id)
+
+	# Search
+	search = request.GET.get('search')
+
+	if search != "" and search is not None:
+		tests = Test.objects.filter(belongs=class_id, name__icontains=search).order_by('-create_time')
+
+
+	# paginator 
+	paginator = Paginator(tests, 5)
+	page = request.GET.get('page', 1)
+
+	try:
+		tests = paginator.page(page)
+	except PageNotAnInteger:
+		tests = paginator.page(1)
+	except EmptyPage:
+		tests = paginator.page(paginator.num_pages)
+
+
+	for t in tests:
+		t.status = "done"
+		
+	room = get_object_or_404(Classroom, id=class_id)
+	return render(request, 'classroom/view_class.html', {'tests' : tests, 'room' : room } )
---
+++ Classroom/views/Teachers.py
@@ -0,0 +1,284 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from ..decorators import teacher_required
+from ..models import Classroom, Enrollment, Test, Question, Answer, testTaken
+from ..forms import CreateTestForm, CreateQnForm, CreateClassForm
+from datetime import datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from django.contrib.auth.models import User
+
+
+@login_required(login_url='login')
+@teacher_required
+def create_class(request):
+	form = CreateClassForm(request.POST or None, request.FILES or None)
+	if request.method == "POST":
+		form = CreateClassForm(request.POST)
+
+		if form.is_valid():
+			name = form.cleaned_data.get('name')
+			desc = form.cleaned_data.get('desc')
+			code = form.cleaned_data.get('code')
+			user = request.user 
+			room = Classroom(owner=user, name=name, code=code, desc=desc)
+			room.save()
+			messages.success(request, '{} Class is Created'.format(room))
+			return redirect('dashboard')
+		else:
+			messages.error(request, form.errors)
+	return render(request, 'teachers/create_class.html', {'form': form})
+
+@login_required(login_url='login')
+@teacher_required
+def update_class(request, class_id):
+	room = get_object_or_404(Classroom, pk=class_id)
+	if request.method == "POST":
+		form = CreateClassForm(request.POST, instance=room)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, '{} Class is Updated'.format(room))
+			return redirect('view_class', class_id)
+		else:
+			messages.error(request, form.errors)
+	else:
+		form = CreateClassForm(instance=room)
+	return render(request, 'teachers/create_class.html', {'form': form, 'room':room})
+
+@login_required(login_url='login')
+@teacher_required
+def delete_class(request, class_id):
+	room = get_object_or_404(Classroom, pk=class_id)
+	messages.success(request, '{} is deleted'.format(room))
+	room.delete()
+	return redirect('dashboard')
+	
+
+@login_required(login_url='login')
+@teacher_required
+def create_test(request, class_id):
+	form = CreateTestForm(request.POST or None, request.FILES or None)
+	if request.method == "POST":
+		form=CreateTestForm(request.POST)
+
+		if form.is_valid():
+			name = form.cleaned_data.get('name')
+			desc = form.cleaned_data.get('desc')
+			start_time = form.cleaned_data.get('start_time')
+			end_time = form.cleaned_data.get('end_time')
+			
+			belongs = get_object_or_404(Classroom, id=class_id)
+
+			test = Test(belongs=belongs, name=name, desc=desc, start_time=start_time, end_time=end_time)
+			test.save()
+
+			return redirect('view_class', class_id)
+		else: 
+			messages.error(request, form.errors)
+
+	return render(request, 'teachers/create_test.html', {'form': form })
+
+@login_required(login_url='login')
+@teacher_required
+def update_test(request, test_id):
+	test = get_object_or_404(Test, pk=test_id)
+
+	if request.method == "POST":
+		form = CreateTestForm(request.POST, instance=test)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, '{}  is Updated'.format(test))
+			return redirect('view_test', test.id)
+		else: 
+			messages.error(request, form.errors)
+	else:
+		form = CreateTestForm(instance=test)
+	return render(request, 'teachers/create_test.html', {'form': form, 'test' : test })
+
+@login_required(login_url='login')
+@teacher_required
+def delete_test(request, test_id):
+	test = get_object_or_404(Test, pk=test_id)
+	room = test.belongs
+	messages.success(request, '{} is deleted'.format(test))
+	test.delete()
+	return redirect('view_class', room.id)
+
+
+
+@login_required(login_url='login')
+@teacher_required
+def view_test(request, test_id):
+	qns = Question.objects.filter(test=test_id)
+	test = get_object_or_404(Test, pk=test_id)
+
+	# Search
+	search = request.GET.get('search')
+
+	if search != "" and search is not None:
+		qns = Question.objects.filter(test=test_id, qn_text__icontains=search)
+
+	# paginator 
+	paginator = Paginator(qns, 5)
+	page = request.GET.get('page', 1)
+
+	try:
+		qns = paginator.page(page)
+	except PageNotAnInteger:
+		qns = paginator.page(1)
+	except EmptyPage:
+		qns = paginator.page(paginator.num_pages)
+
+	return render(request, 'teachers/view_test.html', { 'qns' : qns, 'test' : test } )
+
+
+@login_required(login_url='login')
+@teacher_required
+def students_work(request, test_id):
+	test = get_object_or_404(Test, pk=test_id)
+	student = Enrollment.objects.filter(room=test.belongs).values('student')
+	attended_s = testTaken.objects.filter(test=test_id).values('student')
+	missed_s = [item for item in student if item not in attended_s]
+
+	d = [] 
+	for s in missed_s:
+		d.append(s['student'])
+
+	attended_s = User.objects.filter(pk__in=attended_s).values()
+	missed_s = User.objects.filter(pk__in=d).values()
+
+	for a in attended_s:
+		a['ml_score'] = get_object_or_404(testTaken, test=test_id, student=a['id']).ml_score
+		a['actual_score'] = get_object_or_404(testTaken, test=test_id, student=a['id']).actual_score
+
+
+	qns = Question.objects.filter(test=test_id)
+	test.max_score = 0 
+	
+	for q in qns:
+		test.max_score += q.max_score
+
+	values = {
+		'test': test,
+		'attended_s':attended_s,
+		'missed_s': missed_s,
+	}
+
+	return render(request, 'teachers/students_work.html', values )
+
+
+@login_required(login_url='login')
+@teacher_required
+def individual_work(request, test_id, student_id):	
+	test = get_object_or_404(Test, pk=test_id)
+	student = Enrollment.objects.filter(room=test.belongs).values('student')
+	attended_s = testTaken.objects.filter(test=test_id).values('student')
+	missed_s = [item for item in student if item not in attended_s]
+
+	d = [] 
+	for s in missed_s:
+		d.append(s['student'])
+
+	
+	attended_s = User.objects.filter(pk__in=attended_s).values()
+	missed_s = User.objects.filter(pk__in=d).values()
+	
+	for a in attended_s:
+		a['ml_score'] = get_object_or_404(testTaken, test=test_id, student=a['id']).ml_score
+		a['actual_score'] = get_object_or_404(testTaken, test=test_id, student=a['id']).actual_score
+
+	test.max_score = 0 
+	qns = Question.objects.filter(test=test_id)
+	student = get_object_or_404(User, pk=student_id) 
+	ans = []
+	for q in qns :
+		d = {} 
+		d['qns'] = q 
+		d['ans'] = get_object_or_404(Answer, student=student, question=q)
+		ans.append(d)
+		test.max_score += q.max_score
+	
+
+	values = {
+		'ans' : ans, 
+		'test': test,
+		'student': student, 
+		'attended_s':attended_s,
+		'missed_s': missed_s,
+	}
+
+	return render(request, 'teachers/students_work.html', values )
+
+
+@login_required(login_url='login')
+@teacher_required
+def update_work(request, qn_id, student_id):
+	if request.method == "POST":
+		ac = int(request.POST['actual_score'])
+		student = get_object_or_404(User, pk=student_id) 
+		qn = get_object_or_404(Question, pk=qn_id)
+		ans = get_object_or_404(Answer, student=student, question=qn)
+		tt = get_object_or_404(testTaken, student=student, test=qn.test)
+		tt.actual_score -= ans.actual_score 
+		tt.actual_score += ac 
+		ans.actual_score = ac 
+		ans.save()
+		tt.save() 
+	return redirect('individual_work', qn.test.id, student.id)
+
+
+@login_required(login_url='login')
+@teacher_required
+def create_qn(request, test_id):
+	form = CreateQnForm(request.POST or None, request.FILES or None)
+	if request.method == "POST":
+		form = CreateQnForm(request.POST)
+
+		if form.is_valid():
+			text = form.cleaned_data.get('qn_text')
+			key = form.cleaned_data.get('key')
+			max_score = form.cleaned_data.get('max_score')
+			
+			test = get_object_or_404(Test, id=test_id)
+
+			qn = Question(test=test, qn_text=text, key=key, max_score=max_score)
+			qn.save()
+
+			return redirect('view_test', test_id)
+		else: 
+			messages.error(request, form.errors)
+
+	return render(request, 'teachers/create_qn.html', {'form': form})
+
+@login_required(login_url='login')
+@teacher_required
+def update_qn(request, qn_id):
+	qn = get_object_or_404(Question, pk=qn_id)
+
+	if request.method == "POST":
+		form = CreateQnForm(request.POST, instance=qn)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Question is Updated')
+			return redirect('view_test', qn.test.id)
+		else: 
+			messages.error(request, form.errors)
+	else:
+		form = CreateQnForm(instance=qn)
+	return render(request, 'teachers/create_qn.html', {'form': form, 'qn' : qn })
+
+@login_required(login_url='login')
+@teacher_required
+def delete_qn(request, qn_id):
+	qn = get_object_or_404(Question, pk=qn_id)
+	test = qn.test
+	messages.success(request, 'Question is deleted')
+	qn.delete()
+	return redirect('view_test', test.id)
